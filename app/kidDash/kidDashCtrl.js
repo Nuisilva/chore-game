26d7083088643a28f0e1f9fc29f887fd9a936577
Nui Silva <sigbin2000@gmail.com>
2015-06-25 15:02:45 -0600

    Working on CSS

--- app/kidDash/kidDashCtrl.js
+++ app/kidDash/kidDashCtrl.js
@@ -1,17 +1,10 @@
 var app = angular.module('choreGame');
 
 app.controller ('kidDashCtrl', function($scope, $stateParams, fb, kidRef, $firebaseArray, $firebaseObject, kidService){
-	
-
-	console.log("kidDashCtrl is WORKING")
-	
-
 	$scope.kid = $firebaseObject(kidRef)
 	$scope.kidJobs = $firebaseObject(new Firebase(fb.url + '/kidList/' + $stateParams.kid + '/jobs/'))
-
-
 	$scope.kidJobs.$loaded(function(){
-		console.log($scope.kidJobs)
+		
 	})
 	var test = new Firebase(fb.url + '/kidList/' + $stateParams.kidId )
 
